6e843ae20576509f5786f26cf6819afc326c2473
AvinashTiwari85 <qwe123kids@gmail.com>
2020-01-03 19:42:40 -0700

    no message

--- 3_Nlp_Chatbot/ChatBot.py
+++ 3_Nlp_Chatbot/ChatBot.py
@@ -88,3 +88,10 @@ for word , count in word2count.items():
 
         
     
+tokens = ['<PAD>', 'EOS', '<OUT>', '<SOS>']
+for token in tokens:
+    questionwords2int[token] = len(questionwords2int) + 1
+for token in tokens:
+    answerwords2int[token] = len(answerwords2int) + 1
+    
+answersints2word  = {w_i: w for w,w_i in answerwords2int.items()}
