3de66135f07cbed67b79b3215d6f13996f8fe87f
AvinashTiwari85 <qwe123kids@gmail.com>
2020-01-07 18:12:32 -0700

    no message

--- 3_Nlp_Chatbot/ChatBot.py
+++ 3_Nlp_Chatbot/ChatBot.py
@@ -130,4 +130,18 @@ for length in range(1,26):
             
             
     
+
+def model_input():
+    inputs = tf.placeholder(tf.int32,[None, None], name='input')
+    targets = tf.placeholder(tf.int32,[None, None], name='target')
+    lr = tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    return inputs,targets, lr,keep_prob
+
+def preprocess_targets(targets,word2int, bacthsize):
+    left_side = tf.fill([bacthsize,1],word2int['<SOS>'] )
+    right_side = tf.strided_slice(targets,[0,0],[bacthsize, -1], [1,1])
+    preprocessed_targets = tf.contact([left_side, right_side], 1)
+    return preprocessed_targets
+    
             
--- 3_Nlp_Chatbot/Chatbot1py.py
+++ 3_Nlp_Chatbot/Chatbot1py.py
@@ -4,6 +4,8 @@ Created on Mon Jan  6 18:08:15 2020
 
 @author: avinash.tiwari
 """
+import tensorflow as tf
+
 
 def model_input():
     inputs = tf.placeholder(tf.int32,[None, None], name='input')
@@ -11,3 +13,9 @@ def model_input():
     lr = tf.placeholder(tf.float32, name='learning_rate')
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
     return inputs,targets, lr,keep_prob
+
+def preprocess_targets(targets,word2int, bacthsize):
+    left_side = tf.fill([bacthsize,1],word2int['<SOS>'] )
+    right_side = tf.strided_slice(targets,[0,0],[bacthsize, -1])
+    
+    
