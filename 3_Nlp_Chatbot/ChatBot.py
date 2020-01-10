358117b5ef88c6ddabb2299b89c867b464eb5a6a
AvinashTiwari85 <qwe123kids@gmail.com>
2020-01-09 17:59:40 -0700

    no message

--- 3_Nlp_Chatbot/ChatBot.py
+++ 3_Nlp_Chatbot/ChatBot.py
@@ -143,5 +143,16 @@ def preprocess_targets(targets,word2int, bacthsize):
     right_side = tf.strided_slice(targets,[0,0],[bacthsize, -1], [1,1])
     preprocessed_targets = tf.contact([left_side, right_side], 1)
     return preprocessed_targets
+
+def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    encoder_output, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw = encoder_cell,
+                                                                    cell_bw = encoder_cell,
+                                                                    sequence_length = sequence_length,
+                                                                    inputs = rnn_inputs,
+                                                                    dtype = tf.float32)
+    return encoder_state
     
             
